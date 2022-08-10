15a0ca7272cf0f91d7a60151774a435cde8a2429
Steve Wakhungu <stwakhungu@gmail.com>
2022-08-10 17:39:26 +0300

    Export and import edited for correct app rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import About from "./components/About";
-import Home from "./components/Home";
-import NavBar from "./components/NavBar";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
 
 function App() {
   return (
